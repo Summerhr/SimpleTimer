a4a186f82a89598e7e15ed54a971f71b09d22bd4
haerin <hrparc@naver.com>
2023-05-14 23:06:21 +0900

    Declare properties and methods.

--- simpleTimer/ViewController.swift
+++ simpleTimer/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        
     }
 
+    func setupUI () {
+        mainLabel.text = "시간을 설정하세요."
+        slider.value = 0.0
+    }
+    
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
 
+    @IBAction func startTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 }
 
